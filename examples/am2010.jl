adf2950040453334014a672f4e42e4841f9b62e5
Patrick Kofod Mogensen <patrick.mogensen@gmail.com>
2017-01-13 12:06:06 +0100

    Add MDPTools.

--- examples/am2010.jl
+++ examples/am2010.jl
@@ -6,8 +6,7 @@ bdisc = 0.95 # discount factor
 truepar = [-1.9;1.;2.] # fixed costs, ..., entry costs
 # State 1: Exogenous state
 nX1 = 2
-X1 = [0.;1.]
-F1 = [sparse([1. 0.; 1. 0.]), sparse([0. 1.; 0. 1.])]
+S1 = EntryState(;dense=false)
 
 nX2 = 5
 X2 = 1:nX2
@@ -17,7 +16,7 @@ M = [0.8 0.2 0.0 0.0 0.0;
 	 0.0 0.0 0.2 0.6 0.2;
 	 0.0 0.0 0.0 0.2 0.8;]
 
-S = States(State(X1, F1),
+S = States(S1,
            CommonState(X2, M))
 
 Z = [zeros(nX2*2, 3), # don't buy
@@ -38,9 +37,9 @@ end
 
 estimated_F2 = n_transitions./sum(n_transitions,2)
 
-estimated_S =  States(State(X1, F1), CommonState(X2, estimated_F2))
+estimated_S =  States(S1, CommonState(X2, estimated_F2))
 
 U.Θ*=0.0
 @time am_nfxp = fit_nfxp(U, estimated_S, D)
 U.Θ.*=0.0 # P will get overwritten by the initial non-parametric fitting
-@time am_npl = fit_npl(U, S, D; method = Optim.BFGS(linesearch = LineSearches.morethuente!))
+@time am_npl = fit_npl(U, estimated_S, D; method = Optim.BFGS(linesearch = LineSearches.morethuente!))
--- src/StructuralEstimation.jl
+++ src/StructuralEstimation.jl
@@ -1,6 +1,7 @@
 module StructuralEstimation
 
 using Reexport
+@reexport using StructuralBase
 @reexport using MDPTools
 @reexport using NestedFixedPoint
 @reexport using NestedPseudoLikelihood
